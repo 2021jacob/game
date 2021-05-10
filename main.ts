a0299479e3f7af35cc0776ed76acea367a092e51
2021jacob <83963520+2021jacob@users.noreply.github.com>
2021-05-10 10:18:31 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,63 @@
-
+music.powerUp.play()
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . f . . . . . 3 . . . . . f . 
+    . . f . . . . 3 3 3 . . . . f . 
+    . . f . . . 3 3 3 3 3 . . . f . 
+    . . f . . 3 3 3 3 3 3 3 . . f . 
+    . . f . 3 3 3 3 3 3 3 3 3 . f . 
+    . . f 3 3 3 3 3 3 3 3 3 3 3 f . 
+    . . 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
+    . . f . . . . . . . . . . . f . 
+    `, SpriteKind.Player)
+mySprite.setStayInScreen(true)
+controller.moveSprite(mySprite, 100, 0)
+controller.moveSprite(mySprite, 100, 100)
+scene.setBackgroundColor(8)
+let mySprite2 = sprites.create(img`
+    . . . . . . . . c c c c . . . . 
+    . . . . c c c c c c c c c . . . 
+    . . . c f c c a a a a c a c . . 
+    . . c c f f f f a a a c a a c . 
+    . . c c a f f c a a f f f a a c 
+    . . c c a a a a b c f f f a a c 
+    . c c c c a c c b a f c a a c c 
+    c a f f c c c a b b 6 b b b c c 
+    c a f f f f c c c 6 b b b a a c 
+    c a a c f f c a 6 6 b b b a a c 
+    c c b a a a a b 6 b b a b b a . 
+    . c c b b b b b b b a c c b a . 
+    . . c c c b c c c b a a b c . . 
+    . . . . c b a c c b b b c . . . 
+    . . . . c b b a a 6 b c . . . . 
+    . . . . . . b 6 6 c c . . . . . 
+    `, SpriteKind.Projectile)
+mySprite2.setPosition(50, 0)
+mySprite2.setVelocity(20, 40)
+let mySprite3 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . c c c c . . 
+    . c c c c c . c c c c c f c c . 
+    c c a c c c c c 8 f f c f f c c 
+    c a f a a c c a f f c a a f f c 
+    c a 8 f a a c a c c c a a a a c 
+    c b c f a a a a a c c c c c c c 
+    c b b a a c f 8 a c c c 8 c c c 
+    . c b b a b c f a a a 8 8 c c . 
+    . . . . a a b b b a a 8 a c . . 
+    . . . . c b c a a c c b . . . . 
+    . . . . b b c c a b b a . . . . 
+    . . . . b b a b a 6 a . . . . . 
+    . . . . c b b b 6 6 c . . . . . 
+    . . . . . c a 6 6 b c . . . . . 
+    . . . . . . . c c c . . . . . . 
+    `, SpriteKind.Projectile)
+mySprite3.setPosition(100, 0)
+mySprite3.setVelocity(0, 50)
